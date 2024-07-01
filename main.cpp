8bff4696573289fd98f310f41d72f1f2c691259f
Petrenko Valeriya <mroom9195@gmail.com>
2024-07-01 20:02:44 +0300

    ex3

--- main.cpp
+++ main.cpp
@@ -1,10 +1,50 @@
 #include "struct.h"
 
 int main() {
-    Point p1 = { 0.0, 0.0 };
-    Point p2 = { 3.0, 4.0 };
-	printPoint(p1);
-	printPoint(p2);
-	cout << "Distance between p1 and p2: " << calculateDistance(p1, p2) << '\n';
-	return 0;
+    try {
+        Fraction f1(3, 4);
+        Fraction f2(2, 6);
+		Fraction result;
+        char operation;
+
+        cout << "Fraction 1: " << f1 << endl;
+        cout << "Fraction 2: " << f2 << endl;
+
+        cout << "Enter operation (+, -, *, /): ";
+        cin >> operation;
+
+        switch (operation) {
+        case '+':
+            result = f1 + f2;
+            cout << "Sum: " << result << endl;
+            break;
+        case '-':
+            result = f1 - f2;
+            cout << "Difference: " << result << endl;
+            break;
+        case '*':
+            result = f1 * f2;
+            cout << "Product: " << result << endl;
+            break;
+        case '/':
+            result = f1 / f2;
+            cout << "Quotient: " << result << endl;
+            break;
+        default:
+            cout << "Invalid operation" << endl;
+            break;
+        }
+
+        cout << "Fraction 1 as mixed number: ";
+        f1.toMixedNumber();
+
+        cout << "Fraction 2 as mixed number: ";
+        f2.toMixedNumber();
+
+    }
+    catch (const invalid_argument& e) {
+        cout << e.what() << endl;
+    }
+
+    return 0;
 }
--- struct.h
+++ struct.h
@@ -1,19 +1,79 @@
-#include <cmath>
 #include <iostream>
 
 using namespace std;
 
-struct Point {
-    double x;
-    double y;
+struct Fraction {
+    int numerator;
+    int denominator;
+
+    Fraction(int num = 0, int denom = 1) {
+        if (denom == 0) {
+            throw invalid_argument("Denominator cannot be zero");
+        }
+        numerator = num;
+        denominator = denom;
+        reduce();
+    }
+
+    int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    void reduce() {
+        int gcdValue = gcd(abs(numerator), abs(denominator));
+        numerator /= gcdValue;
+        denominator /= gcdValue;
+        if (denominator < 0) {
+            numerator = -numerator;
+            denominator = -denominator;
+        }
+    }
+
+    void toMixedNumber() const {
+        if (abs(numerator) >= denominator) {
+            int wholePart = numerator / denominator;
+            int remainder = abs(numerator) % denominator;
+            cout << "Mixed number: " << wholePart << " " << remainder << "/" << denominator << endl;
+        }
+        else {
+            cout << "Proper fraction: " << numerator << "/" << denominator << endl;
+        }
+    }
 };
 
-double calculateDistance(const Point& p1, const Point& p2) {
-    double dx = p2.x - p1.x;
-    double dy = p2.y - p1.y;
-    return sqrt(dx * dx + dy * dy);
+Fraction operator+(const Fraction& a, const Fraction& b) {
+    int num = a.numerator * b.denominator + b.numerator * a.denominator;
+    int denom = a.denominator * b.denominator;
+    return Fraction(num, denom);
 }
 
-void printPoint(const Point& p) {
-    cout << "Point(x: " << p.x << ", y: " << p.y << ")\n";
+Fraction operator-(const Fraction& a, const Fraction& b) {
+    int num = a.numerator * b.denominator - b.numerator * a.denominator;
+    int denom = a.denominator * b.denominator;
+    return Fraction(num, denom);
+}
+
+Fraction operator*(const Fraction& a, const Fraction& b) {
+    int num = a.numerator * b.numerator;
+    int denom = a.denominator * b.denominator;
+    return Fraction(num, denom);
+}
+
+Fraction operator/(const Fraction& a, const Fraction& b) {
+    if (b.numerator == 0) {
+        throw invalid_argument("Cannot divide by zero");
+    }
+    int num = a.numerator * b.denominator;
+    int denom = a.denominator * b.numerator;
+    return Fraction(num, denom);
+}
+
+ostream& operator<<(ostream& out, const Fraction& frac) {
+    out << frac.numerator << "/" << frac.denominator;
+    return out;
 }
